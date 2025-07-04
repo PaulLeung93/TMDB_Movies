8db34eafabf8764384c40b3830feea9591ad4e84
Paul Leung <78509075+PaulLeung93@users.noreply.github.com>
2025-07-04 16:50:46 -0400

    fixed: updated Main Activity with proper DI annotation added: bottom nav bar featuring the Home (Feed), Favorites, and Setting screens

--- app/src/main/java/com/example/tmdbmovies/MainActivity.kt
+++ app/src/main/java/com/example/tmdbmovies/MainActivity.kt
@@ -3,45 +3,42 @@ package com.example.tmdbmovies
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.tmdbmovies.presentation.components.BottomNavBar
+import com.example.tmdbmovies.presentation.navigation.AppNavHost
+import com.example.tmdbmovies.presentation.navigation.NavRoutes
 import com.example.tmdbmovies.ui.theme.TMDBMoviesTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             TMDBMoviesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                val navController = rememberNavController()
+                val backStackEntry by navController.currentBackStackEntryAsState()
+                val currentRoute = backStackEntry?.destination?.route ?: NavRoutes.HOME
+
+                Scaffold(
+                    bottomBar = {
+                        BottomNavBar(
+                            currentRoute = currentRoute,
+                            onItemClick = { navController.navigate(it.route) }
+                        )
+                    }
+                ) { innerPadding ->
+                    AppNavHost(
+                        navController = navController,
+                        modifier = Modifier.padding(innerPadding) // ✅ Fix here
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TMDBMoviesTheme {
-        Greeting("Android")
     }
 }
